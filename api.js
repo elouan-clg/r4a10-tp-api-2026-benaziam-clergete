953787b4428e68476b99eb1f100d89665cb32791
Elouan <Elouan.Clerget@etu.univ-grenoble-alpes.fr>
2026-03-16 14:06:23 +0100

    ajout du fichier qui permettra les appels de l'API

---
+++ api.js
@@ -0,0 +1,11 @@
+
+randomCall(){
+//     donne une API random
+//     // https://www.freepublicapis.com/api/random
+//     donne une liste d'api selon critère
+//     // https://freepublicapis.com/api/apis?sort=best&limit=20
+//     donne l'api avec lID donnée
+//     // https://www.freepublicapis.com/api/apis/275
+//     semble pareil que liste API selon critère
+//     // https://www.freepublicapis.com/api/apis?limit=10&sort=best
+}
