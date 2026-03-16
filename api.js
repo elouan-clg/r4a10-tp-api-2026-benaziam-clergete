97ecfc8d193358a5d3bb55ef23e8f1ddbb3fcb3c
Elouan <Elouan.Clerget@etu.univ-grenoble-alpes.fr>
2026-03-16 14:27:38 +0100

    fonctionnement test de l'API par son appel d'une API aléatoire

--- api.js
+++ api.js
@@ -1,11 +1,20 @@
 
-randomCall(){
+function randomCall(){
 //     donne une API random
-//     // https://www.freepublicapis.com/api/random
+//     https://www.freepublicapis.com/api/random
 //     donne une liste d'api selon critère
-//     // https://freepublicapis.com/api/apis?sort=best&limit=20
+//     https://freepublicapis.com/api/apis?sort=best&limit=20
 //     donne l'api avec lID donnée
-//     // https://www.freepublicapis.com/api/apis/275
+//     https://www.freepublicapis.com/api/apis/275
 //     semble pareil que liste API selon critère
-//     // https://www.freepublicapis.com/api/apis?limit=10&sort=best
+//     https://www.freepublicapis.com/api/apis?limit=10&sort=best
+
+    let promiseObj = fetch("https://www.freepublicapis.com/api/random");
+    promiseObj.then( (resp) => resp.json())
+    .then((data) => {
+        //let data = data;
+        console.log(data);
+    })
 }
+
+randomCall();
