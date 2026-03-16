fb6cacbba36d2fbb180d8e34bfeb361aa9f9f620
Elouan <Elouan.Clerget@etu.univ-grenoble-alpes.fr>
2026-03-16 19:07:53 +0100

    changement d'API (bug d'appel)

--- js/api.js
+++ js/api.js
@@ -1,49 +1,21 @@
 
-export class Api {
-   
+
+// export function randomCall() {
+
+//     let promiseObj = fetch("https://www.freepublicapis.com/api/random");
+//     promiseObj.then((resp) => resp.json())
+//         .then((data) => {
+//             console.log(data);
+//         })
+// }
+//randomCall();
+
+function allRegion() {
+
+    let promiseObj = fetch('api.croustillant.menu/v1/regions');
+    promiseObj.then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+        })
 }
 
-
-export function randomCall(){
-    //     donne une API random
-    //     https://www.freepublicapis.com/api/random
-    //     donne une liste d'api selon critère
-    //     https://freepublicapis.com/api/apis?sort=best&limit=20
-    //     donne l'api avec lID donnée
-    //     https://www.freepublicapis.com/api/apis/275
-    //     semble pareil que liste API selon critère
-    //     https://www.freepublicapis.com/api/apis?limit=10&sort=best
-
-        let promiseObj = fetch("https://www.freepublicapis.com/api/random");
-        promiseObj.then( (resp) => resp.json())
-        .then((data) => {
-            //let data = data;
-            console.log(data);
-        })
-    }
-
-    //randomCall();
-    export function SearchCategory(){
-    //     donne une API random
-    //     https://www.freepublicapis.com/api/random
-    //     donne une liste d'api selon critère
-    //     https://freepublicapis.com/api/apis?sort=best&limit=20
-    //     donne l'api avec lID donnée
-    //     https://www.freepublicapis.com/api/apis/275
-    //     semble pareil que liste API selon critère
-    //     https://www.freepublicapis.com/api/apis?limit=10&sort=best
-
-        let promiseObj = fetch("hhttps://freepublicapis.com/api/apis", {
-            method: "Get",
-            body: "data=" + JSON.stringify(userData),
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }
-        );
-        promiseObj.then( (resp) => resp.json())
-        .then((data) => {
-            //let data = data;
-            console.log(data);
-        })
-    }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 import { view } from './vue.js';
-import { api } from './api.js';
+import * as api from "./api.js";
 
+// function allRegion() {
+
+//     let promiseObj = fetch('https://api.croustillant.menu/v1/regions');
+//     promiseObj.then((resp) => resp.json())
+//         .then((data) => {
+//             console.log(data);
+//         })
+// }
 
 view.reserachBtn.addEventListener("click", function () {
   // Informe le modèle du changement
-  let listApi = api.SearchCategory(view.reserachInput);
-  listApi.forEach(uneApi => {
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    div.append(p);
-    p.append(uneApi);
-  });
+  let mesRegions = api.allRegion();//.SearchCategory(view.reserachInput);
 });
